b16af75ce6d01ad6ab5afd4e563cb8c2386d9f44
Manohar sai Ch <115818123+Manohar-1234567890@users.noreply.github.com>
2023-09-27 10:32:34 +0530

    Add files via upload

---
+++ sample.java
@@ -0,0 +1,14 @@
+package pack;
+
+public class sample  {
+
+	public static void main(String[] args)  {
+
+System.out.println("Hi I am Analysing the code");
+System.out.println("Hi I am Analysing the code");
+System.out.println("Hi I am Analysing the code");
+System.out.println("Hi I am Analysing the code");
+System.out.println("Hi I am Analysing the code");
+
+	}
+}
